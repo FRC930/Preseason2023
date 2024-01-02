2710fec3ca3e813243d3563a746466cc046abeeb
MattCooke <cooke_matthew@hotmail.com>
2024-01-01 19:26:01 -0600

    Initial work on creating unit tests.

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Preseason2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 30;
+  public static final String GIT_SHA = "d64120b83419ec1f177db8da757443930aa675dd";
+  public static final String GIT_DATE = "2023-12-20 14:14:20 CST";
+  public static final String GIT_BRANCH = "master";
+  public static final String BUILD_DATE = "2024-01-01 18:40:22 CST";
+  public static final long BUILD_UNIX_TIME = 1704156022684L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
---
+++ src/test/java/CRT_Tests.java
@@ -0,0 +1,5 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CRT_Tests {
+    
+}
